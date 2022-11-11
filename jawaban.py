ad55105ef798c819734f4042a88f939a25b75abc
rifki227 <115854733+rifki227@users.noreply.github.com>
2022-11-11 09:25:02 +0700

    Create jawaban.py

---
+++ jawaban.py
@@ -0,0 +1,13 @@
+nama_depan = "Rifki"
+nama_belakang = 'Arif Rahman'
+nama_lengkap = nama_depan + ' ' + nama_belakang
+usia = '19'
+alamat = 'Gadog'
+kata_mutiara = "carilah ilmu dimana saja"
+
+print(nama_lengkap, '(' + usia + ')', ',', 'dari', alamat, ', kata mutiara:', kata_mutiara)
+
+print('\nTipe dari nama_lengkap:', type(nama_lengkap))
+print('Tipe dari usia:', type(usia))
+print('Tipe dari alamat:', type(alamat))
+print('Tipe dari kata_mutiara:', type(kata_mutiara))
